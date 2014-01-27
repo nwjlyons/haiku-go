b6e66a2a1da0cc4549cfa55793a4c99df9ee9653
Neil Lyons <nwjlyons@googlemail.com>
2014-01-27 20:43:18 +0000

    Added go program

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-haiku
+Haiku
 =====
 
-Random name generator
+Haiku (meaning a very short form of [Japanese poetry](http://en.wikipedia.org/wiki/Haiku)) is a random name generator.
+
+## Install
+
+    go get github.com/nwjlyons/haiku
+
+This is an example of what happens when you run `haiku`:
+
+
+    haiku
+    autumn-field
+
+Inspired from this gist https://gist.github.com/afriggeri/1266756
---
+++ haiku.go
@@ -0,0 +1,42 @@
+// Haiku is a random name generator
+//
+// Haiku is a very short form of Japanese poetry http://en.wikipedia.org/wiki/Haiku
+//
+// Inspired from this gist https://gist.github.com/afriggeri/1266756
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	adjs = []string{
+		"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+		"summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+		"patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+		"billowing", "broken", "cold", "damp", "falling", "frosty", "green",
+		"long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
+		"red", "rough", "still", "small", "sparkling", "throbbing", "shy",
+		"wandering", "withered", "wild", "black", "young", "holy", "solitary",
+		"fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
+		"polished", "ancient", "purple", "lively", "nameless"}
+	nouns = []string{
+		"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+		"snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+		"forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+		"butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+		"feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+		"snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+		"violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+		"wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
+		"frog", "smoke", "star"}
+)
+
+// Main prints a random name to stdout.
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Printf("%s-%s\n", adjs[rand.Intn(len(adjs))], nouns[rand.Intn(len(nouns))])
+}
